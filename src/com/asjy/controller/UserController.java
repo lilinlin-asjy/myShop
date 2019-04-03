65978fcef8110608c1233d9ed82a2579774b33ca
lilinlin-asjy <49183590+lilinlin-asjy@users.noreply.github.com>
2019-04-03 11:10:18 +0800

    Update UserController.java

--- src/com/asjy/controller/UserController.java
+++ src/com/asjy/controller/UserController.java
@@ -3,5 +3,7 @@ package com.asjy.controller;
 public class UserController {
     public void getUser(){
         System.out.println("get le user");
+        System.out.println("git edit over");
+        
     }
 }
